f787124f72af0e8c8a08dbadb80927934d717ebb
Cliff Saporta Cheng <ccfcheng@gmail.com>
2016-06-18 20:00:24 -0700

    feat(setup): Setup initial dependencies (#3)
    - Closes #1

---
+++ test/test.spec.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+
+describe('Test', function() {
+  it('Should pass,', function() {
+    expect(1).to.equal(1);
+  });
+
+  it('Should also pass,', function() {
+    expect(1).to.equal(1);
+  });
+
+});
